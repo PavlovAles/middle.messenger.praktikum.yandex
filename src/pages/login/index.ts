8af4b401ec0573daa731ce3daddeda6bfa62cc20
Ales Pavlov <pavlov.a@ad.eyetronic.ru>
2024-04-03 21:05:56 +0300

    refactor login and registration pages

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -1,2 +1,44 @@
+import Handlebars from 'handlebars';
 import './login.css';
 export { default as LoginPage } from './login.hbs?raw';
+
+const loginFormInputs = [
+    {
+        type: "text",
+        label: "Логин",
+        name: "name",
+        errorMessage: "oops",
+        required: true,
+    },
+    {
+        type: "password",
+        label: "Пароль",
+        name: "password",
+        errorMessage: "oops",
+        required: true,
+    }
+];
+
+const loginFormButtons = [
+    {
+        type: "submit",
+        variant: "primary",
+        fill: "solid",
+        text: "Войти",
+        align: 'center',
+    },
+    {
+        type: "button",
+        variant: "primary",
+        fill: "link",
+        text: "Нет аккаунта?",
+    }
+];
+
+Handlebars.registerHelper('loginFormInputs', () => {
+    return loginFormInputs;
+});
+
+Handlebars.registerHelper('loginFormButtons', () => {
+    return loginFormButtons;
+}) 
--- src/pages/registration/index.ts
+++ src/pages/registration/index.ts
@@ -1,2 +1,68 @@
+import Handlebars from 'handlebars';
 import './registration.css';
 export { default as RegistrationPage } from './registration.hbs?raw';
+
+const registrationFormInputs = [
+    {
+        type: "email",
+        label: "Почта",
+        name: "email",
+        required: true,
+    },
+    {
+        type: "text",
+        label: "Логин",
+        name: "login",
+        required: true,
+    },
+    {
+        type: "text",
+        label: "Имя",
+        name: "first_name",
+    },
+    {
+        type: "text",
+        label: "Фамилия",
+        name: "second_name",
+    },
+    {
+        type: "text",
+        label: "Телефон",
+        name: "phone",
+    },
+    {
+        type: "text",
+        label: "Пароль",
+        name: "password",
+        required: true,
+    },
+    {
+        type: "text",
+        label: "Пароль (еще раз)",
+        name: "password_confirm",
+        required: true
+    },
+];
+
+const registrationFormButtons = [
+    {
+        type: "submit",
+        variant: "primary",
+        fill: "solid",
+        text: "Зарегистрироваться",
+    },
+    {
+        type: "button",
+        variant: "primary",
+        fill: "link",
+        text: "Войти",
+    }
+];
+
+Handlebars.registerHelper('registrationFormInputs', () => {
+    return registrationFormInputs;
+});
+
+Handlebars.registerHelper('registrationFormButtons', () => {
+    return registrationFormButtons;
+})
