c82fb416a6fc26cf665e650b894104362294c2b0
Ales Pavlov <pavlov.a@ad.eyetronic.ru>
2024-04-03 21:05:06 +0300

    refactor box -> ShadowBox

--- src/helpers/box/box.ts
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import styles from './box.module.css';
-
-function box(options: any) {
-    //@ts-ignore
-    return new Handlebars.SafeString(`<div class="${styles.box}">${options.fn(this)}</div>`);
-}
-
-export default box;
