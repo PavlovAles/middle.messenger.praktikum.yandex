dbfb75c16b62675bac8736ad7bfbbff68fdb7ea6
Ales Pavlov <pavlov.a@ad.eyetronic.ru>
2024-04-04 22:40:42 +0300

    refactor dummy routing

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,12 @@
 import Handlebars from 'handlebars';
 import * as Components from './components';
-import * as Pages from './pages';
-
-const pages = {
-    'login': [Pages.LoginPage, Pages.getLoginPageContext('login')],
-    'registration': [
-        Pages.LoginPage,
-        Pages.getLoginPageContext('registration'),
-    ],
-    'main': [Pages.MainPage],
-    'profile': [Pages.ProfilePage, Pages.getProfilePageContext('info')],
-    'profile-change-info': [
-        Pages.ProfilePage,
-        Pages.getProfilePageContext('changeInfo'),
-    ],
-    'profile-change-password': [
-        Pages.ProfilePage,
-        Pages.getProfilePageContext('changePassword'),
-    ],
-    'error-404': [Pages.ErrorPage, Pages.getErrorPageContext('404')],
-    'error-500': [Pages.ErrorPage, Pages.getErrorPageContext('500')],
-    'nav': [Pages.NavigatePage],
-    'test': [Pages.TestPage],
-};
+import { pages } from './constants/pages';
 
 Object.entries(Components).forEach(([name, component]) => {
     Handlebars.registerPartial(name, component);
 });
 
 function navigate(page: string) {
-    //@ts-ignore
     const [source, context] = pages[page];
     const container = document.getElementById('app')!;
     container.innerHTML = Handlebars.compile(source)(context);
